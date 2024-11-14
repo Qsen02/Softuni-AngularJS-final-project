76392773e799e5ccf4f08725fdc6af0996f7c847
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-14 20:25:06 +0200

    Implement show password functionallity

--- Client/src/app/main/register/register.component.ts
+++ Client/src/app/main/register/register.component.ts
@@ -9,5 +9,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+   isVisible=false;
+  
+   changeVisability(event:Event){
+    const target =event.target as HTMLElement
+      const inputRef=target?.parentElement?.children[2] as HTMLInputElement;
+      if(inputRef.type=="password"){
+        inputRef.type="text";
+      }else{
+        inputRef.type="password";
+      }
+   }
 }
