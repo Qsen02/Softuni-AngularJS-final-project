ce2ef411135e0f279d939f40666e436e0fe907d0
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-14 16:31:34 +0200

    Implemented error handling on posts and loading spinner

--- Client/src/app/main/main.component.ts
+++ Client/src/app/main/main.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { Post } from '../types/post';
 import { PostsService } from '../services/posts.service';
 import { MainPostsComponent } from './main-posts/main-posts.component';
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [MainPostsComponent],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+	selector: 'app-main',
+	standalone: true,
+	imports: [MainPostsComponent],
+	templateUrl: './main.component.html',
+	styleUrl: './main.component.css'
 })
 export class MainComponent implements OnInit {
-  posts: Post[] = [];
+	posts: Post[] = [];
+	isLoading = signal(false);
+	isError = signal(false);
 
-  constructor(private postApi: PostsService) { }
+	constructor(private postApi: PostsService) { }
 
-  ngOnInit(): void {
-    this.postApi.getAllPosts().subscribe((posts) => {
-      this.posts = posts;
-      console.log(this.posts)
-    })
-  }
+	ngOnInit(): void {
+		this.isLoading.set(true);
+		this.postApi.getAllPosts().subscribe({
+			next: (posts) => {
+				this.posts = posts;
+				this.isLoading.set(false);
+			},
+			error: err => {
+				this.isError.set(true);
+				console.log(this.isError());
+			}
+		});
+	}
 }
