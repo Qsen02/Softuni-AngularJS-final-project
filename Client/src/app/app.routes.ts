50d805088a608755f117bebd61d70198248214ac
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-14 17:06:24 +0200

    Implement register component without styling

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ErrorPageComponent } from './main/error-page/error-page.component';
+import { RegisterComponent } from './main/register/register.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full" },
     { path: "home", component: MainComponent },
-    { path: "**", component: ErrorPageComponent }
+    { path: "register", component:RegisterComponent},
+    { path: "**", component: ErrorPageComponent },
 ];
