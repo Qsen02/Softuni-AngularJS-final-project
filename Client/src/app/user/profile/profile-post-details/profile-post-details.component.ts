c765af345dd60eea2028428c22cda92472b451e1
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-25 14:57:16 +0200

    Implemented loading spinner in profile post details

--- Client/src/app/user/profile/profile-post-details/profile-post-details.component.ts
+++ Client/src/app/user/profile/profile-post-details/profile-post-details.component.ts
@@ -3,6 +3,8 @@ import { Post } from '../../../types/post';
 import { PostsService } from '../../../services/posts.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProfilePostCommentsComponent } from './profile-post-comments/profile-post-comments.component';
+import { AuthUser } from '../../../types/user';
+import { UserService } from '../../../services/user.service';
 
 @Component({
     selector: 'app-profile-post-details',
@@ -15,7 +17,12 @@ export class ProfilePostDetailsComponent implements OnInit{
     post: Post | null = null;
     isLoading=false;
     isError=false;
-    constructor(private postService:PostsService, private route:ActivatedRoute,private router:Router){}
+    curUser:AuthUser|null=null;
+    constructor(private postService:PostsService, 
+        private route:ActivatedRoute,
+        private router:Router,
+        private userService:UserService
+    ){}
 
     ngOnInit(): void {
         this.isLoading=true;
@@ -23,6 +30,7 @@ export class ProfilePostDetailsComponent implements OnInit{
         this.postService.getPostById(postId).subscribe({
             next:(post)=>{
                 this.post=post;
+                this.curUser=this.userService.getUser();
                 this.isLoading=false;
             },
             error:(err)=>{
