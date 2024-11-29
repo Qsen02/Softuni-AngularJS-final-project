42c9a53599b385cdfa04eedd85634a0450d90415
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-29 11:01:24 +0200

    Post service updated

--- Client/src/app/services/posts.service.ts
+++ Client/src/app/services/posts.service.ts
@@ -2,26 +2,39 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { enviroment } from '../enviroment/app.enviroment';
 import { Post } from '../types/post';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class PostsService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     getAllPosts() {
         return this.http.get<Post[]>("/api/posts");
     }
 
     getPostById(id: string) {
-        return this.http.get<Post>(`/api/posts/${id}`);
+        return this.http.get<Post>(`/api/posts/${id}`)
     }
 
     likePost(postId: string | undefined) {
-        return this.http.post(`/api/posts/${postId}/like`, {}).subscribe();
+        return this.http.post(`/api/posts/${postId}/like`, {})
     }
 
     unlikePost(postId: string | undefined) {
-        return this.http.post(`/api/posts/${postId}/unlike`, {}).subscribe();
+        return this.http.post(`/api/posts/${postId}/unlike`, {})
+    }
+
+    createPost(description: string, imageUrl: string) {
+        return this.http.post<Post>(`/api/posts`, { description, imageUrl });
+    }
+
+    deletePost(postId: string) {
+        return this.http.delete(`/api/posts/${postId}`);
+    }
+
+    updatePost(postId:string,data:{}){
+        return this.http.put(`/api/posts/${postId}`,data);
     }
 }
