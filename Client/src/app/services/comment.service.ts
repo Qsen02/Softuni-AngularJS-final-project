9aba3504895fb2e4717d83feeb33cbb758c27715
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-30 15:38:30 +0200

    Comment service updated

--- Client/src/app/services/comment.service.ts
+++ Client/src/app/services/comment.service.ts
@@ -13,4 +13,24 @@ export class CommentService {
     getCommentById(commentId: string | undefined): Observable<Comment> {
         return this.http.get<Comment>(`/api/comments/${commentId}`);
     }
+
+    createComment(postId:string,content:string){
+        return this.http.post<Comment>(`/api/comments/in/${postId}`,{content});
+    }
+
+    editComment(commentId:string,content:string){
+        return this.http.put(`/api/comments/${commentId}`,{content});
+    }
+
+    deleteComment(commentId:string,postId:string){
+        return this.http.delete(`/api/comments/${commentId}/in/${postId}`);
+    }
+
+    likeComment(commentId:string){
+        return this.http.post(`/api/comments/${commentId}/like`,{});
+    }
+
+    unlikeComment(commentId:string){
+        return this.http.post(`/api/comments/${commentId}/unlike`,{});
+    }
 }
