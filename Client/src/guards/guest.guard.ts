f0f08d13506ad09bc78d231aad1c03fa6c59b196
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-12-02 10:28:36 +0200

    Guest guard updated

--- Client/src/guards/guest.guard.ts
+++ Client/src/guards/guest.guard.ts
@@ -1,14 +1,20 @@
 import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { UserService } from "../app/services/user.service";
+import { map } from "rxjs";
 
 export const guestGuard: CanActivateFn = () => {
     const userService = inject(UserService);
-    const isLogged = userService.isLogged;
-    const router=inject(Router);
-    if (isLogged) {
-        router.navigate(["/home"]);
-        return false;
-    }
-    return true;
+    const router = inject(Router);
+
+    return userService.getUserProfile().pipe(
+        map((user) => {
+            if (user) {
+                console.log(user);
+                router.navigate(['/home']);
+                return false;
+            }
+            return true;
+        })
+    )
 }
