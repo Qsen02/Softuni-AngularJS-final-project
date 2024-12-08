b06ba1b03e90f9e57205728602cd30229ca7536c
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-12-08 16:58:49 +0200

    Udpate environments

--- Client/src/app/interceptors/auth.interceptor.ts
+++ Client/src/app/interceptors/auth.interceptor.ts
@@ -1,20 +1,21 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
-import { enviroment } from '../enviroment/app.enviroment';
+import { enviroment } from '../../../enviroment/app.enviroment';
 import { inject } from '@angular/core';
 import { ErrorMessageService } from '../services/error-message.service';
 import { Router } from '@angular/router';
+import { production } from '../../../enviroment/production.environment';
 
-const { apiUrl } = enviroment;
-const API = "/api";
+// const { apiUrl } = enviroment;
+const { apiUrl } = production;
+const API = '/api';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-
     if (req.url.startsWith(API)) {
         req = req.clone({
             url: req.url.replace(API, apiUrl),
-            withCredentials: true
-        })
+            withCredentials: true,
+        });
     }
 
     const errorService = inject(ErrorMessageService);
@@ -25,11 +26,11 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
             if (err.status === 401) {
                 router.navigate(['/login']);
             } else if (err.status === 403) {
-                localStorage.removeItem("user");
+                localStorage.removeItem('user');
             } else {
                 errorService.setError(err);
             }
-            return throwError(()=>err);
+            return throwError(() => err);
         })
     );
 };
