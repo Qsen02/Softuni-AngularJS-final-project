2845645377609f5973aa341eb9cc9ac8d3ce037f
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-12-08 15:39:32 +0200

    Update

--- Server/src/config/mongoose.js
+++ Server/src/config/mongoose.js
@@ -6,7 +6,7 @@ const {Comments}=require("../models/comment");
 const localDB="mongodb://127.0.0.1:27017/Social-media";
 
 async function runDB(){
-    await mongoose.connect(localDB);
+    await mongoose.connect(process.env.PRODUCTION_DB);
     console.log("Database is running!");
 }
 
