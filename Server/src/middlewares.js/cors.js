1a6268b933ed375690a0fb86b8a64c07ef760217
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-12-09 22:59:48 +0200

    Cors updated

--- Server/src/middlewares.js/cors.js
+++ Server/src/middlewares.js/cors.js
@@ -1,6 +1,6 @@
 function setCors(){
     return function(req,res,next){
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.setHeader("Access-Control-Allow-Origin", ["http://localhost:4200","https://softuni-angularjs-final-project.onrender.com"]);
         res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, DELETE, POST");
         res.setHeader("Access-Control-Allow-Headers", "Content-type, X-Authorization");
         res.setHeader("Access-Control-Allow-Credentials",true);
