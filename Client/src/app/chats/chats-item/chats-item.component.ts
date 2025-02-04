0b2f47c2b3d495d9898f404005a6aaac892409c5
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-02-04 23:19:38 +0200

    CSS update on chat-item component

--- Client/src/app/chats/chats-item/chats-item.component.ts
+++ Client/src/app/chats/chats-item/chats-item.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Chat } from '../../types/chats';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { imageProfileErrorHandler } from '../../utils/imageErrorHandlers';
 
 @Component({
     selector: 'app-chats-item',
@@ -13,4 +14,9 @@ import { CommonModule } from '@angular/common';
 export class ChatsItemComponent {
     @Input('chatProp') chat: Chat | null = null;
     @Input('userIdProp') userId = '';
+
+     onProfileImageError(event: Event) {
+            const imageRef = event.target as HTMLImageElement;
+            imageProfileErrorHandler(imageRef);
+    }
 }
